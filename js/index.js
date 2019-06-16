d0f349d4fa3930fcc605d13301cedd0679e17897
ArthurGoupil <arthurgoupil@gmail.com>
2019-06-17 00:54:39 +0200

    Popover for comment + set up localStorage + fix row number html table

--- js/index.js
+++ js/index.js
@@ -1,34 +1,37 @@
-$(document).ready(function(){
+$(document).ready(function(){ 
     const todoList = [];
     const getUniqueId = function() {
         return 'id-' + Math.random().toString(36).substr(2, 16);
       };
     const tbodyElement = $('#todoTable tbody');
     const generateRow = function (id, todoIndex, toDo, importance, comments) {
+        $(document).ready(function(){
+            $('[data-toggle="popover"]').popover({
+                placement:'top',
+                trigger:'hover focus'
+            });
+        });
         return '<tr id="'+ id +'">' +
-            '<td>' + todoIndex + '</td>' +
+            '<td class"rowIndex">' + todoIndex + '</td>' +
             '<td>' + toDo + '</td>' +
             '<td>' + importance + '</td>' +
-            '<td>' + comments + '</td>' +
+            '<td><button type="button" class="comments btn btn-secondary" data-toggle="popover" data-content="' + comments + '">?</button></td>' +
             '<td><button type="button" class="deleteButton btn btn-danger">Delete</button></td>' +
         '</tr>'
     };
+    
+    for (i=localStorage.length-1 ; i >= 0 ; i--) {
+        let savedObject = JSON.parse(localStorage.getItem(localStorage.key(i)))
+        todoList.push(savedObject);
+        const todoIndex = todoList.indexOf(savedObject) + 1;
+        $(tbodyElement).append(generateRow(savedObject.id, todoIndex, savedObject.toDo, savedObject.importance, savedObject.comments));
+    };
+
     const clearForm = function() {
         $('#todoName').val('');
         $('#todoImportance').val('...');
         $('#todoComments').val(''); 
         $('#nbLetters').html('100 caracters left.');
-    };
-
-    const findIndexObject = function(array, attr, value) {
-        return array.findIndex(element => element[attr] === value);
-        /*
-        for(var i = 0; i < array.length; i += 1) {
-            if(array[i][attr] === value) {
-                return i;
-            };
-        };
-        */ 
     };
 
     $('#todoName').keyup(function() {
@@ -62,35 +65,29 @@ $(document).ready(function(){
         const todoIndex = todoList.indexOf(todoObject) + 1;
         $(tbodyElement).append(generateRow(todoObject.id, todoIndex, todoObject.toDo, todoObject.importance, todoObject.comments));
         clearForm();
+        let todoObjectJson = JSON.stringify(todoObject);
+        localStorage.setItem(todoObject.id,todoObjectJson);
+
+        /*
+        $('.deleteButton').click(function() {
+            console.log('click')
+            const indexObject = todoList.findIndex(element => element["id"] === $(this).closest('tr').attr('id'));
+            todoList.splice(indexObject, 1);
+            $(this).closest('tr').remove();
+        });
+        */
+
+
     });
 
     $("#todoTable").on('click', '.deleteButton', function () {
-        const indexObject = findIndexObject(todoList,"id",$(this).closest('tr').attr('id'));
+        const indexObject = todoList.findIndex(element => element["id"] === $(this).closest('tr').attr('id'));
         todoList.splice(indexObject, 1);
         $(this).closest('tr').remove();
+        let rows = document.querySelectorAll('tr');
+        localStorage.removeItem($(this).closest('tr').attr('id'));
+        rows.forEach((row) => {
+            $(row.firstChild).html(row.rowIndex);
+        });
     });
 });
-
-/*        $(this).closest('tr').remove();
-document.getElementById("myLI").parentElement.nodeName
-*/
-
-/* fonction generate tr
-fonction comptage de caractere */
-
-/* pour récupération cookies
-        todoList.forEach(todoObject => {
-            const todoIndex = todoList.indexOf(todoObject) + 1;
-            $('#todoTable tbody').append(
-                '<tr>' +
-                    '<td>' + todoIndex + '</td>' +
-                    '<td>' + todoObject.toDo + '</td>' +
-                    '<td>' + todoObject.importance + '</td>' +
-                    '<td>' + todoObject.comments + '</td>' +
-                '</tr>'
-                );
-            $('#todoName').val('');
-            $('#todoImportance').val('...');
-            $('#todoComments').val(''); 
-        });
-*/
